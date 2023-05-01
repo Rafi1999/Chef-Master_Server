7a1cd668372d72e4a6fba7bc5c707f7bc8a73c0f
Al Rafi <60866655+Rafi1999@users.noreply.github.com>
2023-05-02 00:49:14 +0600

    second

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const express= require('express')
+
 const app = express();
+
+app.get("/", (res,req)=>{
+    res.json({message: 'Hello'});
+});
+
+app.get("/chefData", (res,req)=>{
+    res.send({result: 'Hello chef'});
+});
+
+app.listen(5000,()=>{
+
+})
